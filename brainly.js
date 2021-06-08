2448c8ef26803deee424801effc847560e5b98fd
aqulzz <75623219+zennn08@users.noreply.github.com>
2021-06-08 10:30:57 +0700

    Update brainly.js

--- brainly.js
+++ brainly.js
@@ -1,4 +1,5 @@
-const brainly = require('brainly-scraper')
+const brainly = require('brainly-scraper');
+const Brenly = require('brainly-scraper-v2');
 
 module.exports = BrainlySearch = (pertanyaan, jumlah, cb) => {
     brainly(pertanyaan.toString(), Number(jumlah)).then((res) => {
@@ -20,6 +21,23 @@ module.exports = BrainlySearch = (pertanyaan, jumlah, cb) => {
     }).then(x => {
         cb(x)
     }).catch(err => {
-        console.log(err.error)
+        brainly(pertanyaan.toString(), Number(jumlah)), "id").then(res => {
+            console.log(res);
+            let brainlyResulti = []
+            res.data.forEach((ask) => {
+                let opt = {
+                    pertanyaan: ask.pertanyaan,
+                    fotoPertanyaan: ask.questionMedia
+                }
+                ask.jawaban.forEach(answer => {
+                    opt.jawaban = {
+                        judulJawaban: answer.text,
+                        fotoJawaban: answer.media
+                    }
+                })
+                brainlyResulti.push(opt)
+            })
+            return brainlyResulti
+        });
     })
 }
